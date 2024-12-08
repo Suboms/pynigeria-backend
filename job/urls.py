fe5e6a6addb4cf4efcbd79db2172a00602487217
DamaMichaelYohanna <get2dama11@gmail.com>
2024-12-09 00:29:18 +0100

    update url path  names

--- job/urls.py
+++ job/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .apis import JobPostingCreateView, JobPostingDetailView, JobPostingListView, JobPostingUpdateView, JobPostingDeleteView
 
+app_name = "job"
 
 urlpatterns = [
-    path('', JobPostingListView.as_view(), name='job-posting-list'),
-    path('postings/', JobPostingCreateView.as_view(), name='job-posting-create'),
-    path('postings/<slug:slug>/', JobPostingDetailView.as_view(), name='job-posting-detail'),
-    path('postings/<int:pk>/update', JobPostingUpdateView.as_view(), name='job-posting-upate'),
-    path('postings/<int:pk>/delete', JobPostingDeleteView.as_view(), name='job-posting-delete'),
+    path('', JobPostingListView.as_view(), name='job_posting_list'),
+    path('postings/', JobPostingCreateView.as_view(), name='job_posting_create'),
+    path('postings/<slug:slug>/', JobPostingDetailView.as_view(), name='job_posting_detail'),
+    path('postings/<int:pk>/update', JobPostingUpdateView.as_view(), name='job_posting_upate'),
+    path('postings/<int:pk>/delete', JobPostingDeleteView.as_view(), name='job_posting_delete'),
     
 ]
