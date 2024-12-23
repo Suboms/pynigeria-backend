430075f92d35062e724766b63407d7f17f98376c
Donchuks02 <chukwudidavid02@gmail.com>
2024-12-23 06:52:48 +0100

    Add UserUpload model with custom manager, uploads status validation and file extension validation. Also modified the permissions.py to use the projects auth in the settings

--- pynigeriaBackend/settings.py
+++ pynigeriaBackend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "job_listing_api.apps.JobApiConfig",
     "job.apps.JobConfig",
     "knowledge_base_api.apps.KnowledgeBaseApiConfig",
+    "taggit"
 ]
 
 MIDDLEWARE = [
@@ -157,6 +158,10 @@ REST_FRAMEWORK = {
     },
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "EXCEPTION_HANDLER": "pynigeriaBackend.exception_handler.pynigeria_exception_handler",
+
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
